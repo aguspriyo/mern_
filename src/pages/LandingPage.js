3542138e78e7493bf5949369f4bcb42ced05645c
aguspriyo <agusppriiyono@gmail.com>
2023-12-21 16:22:47 +0700

    update mostpicked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,16 +1,25 @@
+/* eslint-disable no-undef */
+import landingPage from "json/landingPage.json";
+import Categories from "parts/Categories";
 import Header from "parts/Header";
-import { Component } from "react";
-
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+import React, { Component } from "react";
 
 export default class LandingPage extends Component {
+  constructor(props){
+    super(props);
+    this.refMostPicked = React.createRef()
+  }
   render(){
-console.log(this.props);
+// console.log(this.props);
 
   return (
     <>
-    <Header {...this.props}>
-
-    </Header>
+    <Header {...this.props}></Header>
+    <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}/>
+    <MostPicked refMostPicked ={this.refMostPicked} data={landingPage.mostPicked}/>
+    <Categories data={landingPage.categories}/>
     </>
   )
 }
--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -1,9 +1,9 @@
-import Button from 'elements/Button';
+import Button from '../elements/Button';
 
 
 export default function BrandIcon() {
   return (
-    <Button className="brand-text-icon" href="" type='link'>
+    <Button className="brand-text-icon" href="" type="link">
       Stay<span className="text-gray-900">cation.</span>
     </Button>
   )
